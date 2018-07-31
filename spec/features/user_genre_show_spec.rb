a5004f00b970ae5ee429cef3db38dc1ba6ead02e
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-31 10:47:25 -0600

    all user stories complete, broke cart test while reorganizing admin page - reworking

---
+++ spec/features/user_genre_show_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe 'a user' do
+  describe 'visits a genre show page' do
+    it 'sees all genres' do
+      artist = Artist.create(name: 'some guy')
+      song_1 = Song.create(title: 'Muh', length: 4, play_count: 78, artist_id: 1, rating: 5)
+      song_2 = Song.create(title: 'Suh', length: 4, play_count: 78, artist_id: 1, rating: 5)
+      song_3 = Song.create(title: 'Wuh', length: 4, play_count: 78, artist_id: 1, rating: 5)
+      genre_1 = Genre.create(name: 'Ya like jazz?')
+      genre_2 = Genre.create(name: 'Speed Metal')
+      song_genre_1 = SongGenre.create(song_id: 1, genre_id: 1)
+      song_genre_2 = SongGenre.create(song_id: 2, genre_id: 1)
+      song_genre_3 = SongGenre.create(song_id: 3, genre_id: 2)
+
+      visit genres_path
+
+      click_on genre_1.name
+
+      expect(current_path).to eq(genre_path(genre_1))
+      expect(page).to have_content(genre_1.name)
+      expect(page).to have_content(song_1.title)
+      expect(page).to have_content(song_2.title)
+      expect(page).to_not have_content(song_3.title)
+    end
+    it 'sees average rating of all songs in genre' do
+      artist = Artist.create(name: 'some guy')
+      song_1 = Song.create(title: 'Muh', length: 4, play_count: 78, artist_id: 1, rating: 3)
+      song_2 = Song.create(title: 'Suh', length: 4, play_count: 78, artist_id: 1, rating: 5)
+      genre = Genre.create(name: 'Ya like jazz?')
+      song_genre_1 = SongGenre.create(song_id: 1, genre_id: 1)
+      song_genre_2 = SongGenre.create(song_id: 2, genre_id: 1)
+      expected_number = 4
+
+      visit genre_path(genre)
+
+      expect(page).to have_content("Average rating: #{expected_number}")
+    end
+  end
+end
--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -9,4 +9,18 @@ describe Genre, type: :model do
     it {should have_many(:song_genres)}
     it {should have_many(:songs).through(:song_genres)}
   end
+
+  describe 'instance methods' do
+    it 'calculates average rating of all songs in genre' do
+      artist = Artist.create(name: 'some guy')
+      song_1 = Song.create(title: 'Muh', length: 4, play_count: 78, artist_id: 1, rating: 3)
+      song_2 = Song.create(title: 'Suh', length: 4, play_count: 78, artist_id: 1, rating: 5)
+      genre = Genre.create(name: 'Ya like jazz?')
+      song_genre_1 = SongGenre.create(song_id: 1, genre_id: 1)
+      song_genre_2 = SongGenre.create(song_id: 2, genre_id: 1)
+      expected_number = 4
+
+      expect(genre.average_song_rating).to eq(4)
+    end
+  end
 end
